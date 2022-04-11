a76674798b55ce8cfcbf2c03b8bf25ddd353e715
윤철중 <cjfwnd123zz@gmail.com>
2022-04-11 12:38:22 +0900

    재귀 관계식을 통한 구현

--- src/ChainedMatrixMultiplications.java
+++ src/ChainedMatrixMultiplications.java
@@ -3,6 +3,7 @@ import java.util.*;
 public class ChainedMatrixMultiplications {
     private static int[] d;
     private static int[][] m;
+    final static int INF = Integer.MAX_VALUE / 2; // 양의 무한대
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -13,5 +14,27 @@ public class ChainedMatrixMultiplications {
         for(int i = 0; i < n; i++) {
             d[i] = scanner.nextInt();
         }
+
+        for (int L = 0; L < n-1; L++){
+            for (int i = 1; i <= n-1-L; i++) {
+                int j = i + L;
+                if (i == j){
+                    m[i][j] = 0;
+                    continue;
+                }
+                m[i][j] = INF;
+                for (int k = i; k <= j-1; k++){
+                    m[i][j] = Math.min(m[i][j], m[i][k] +m[k+1][j] + d[i-1]*d[k]*d[j]);
+                    System.out.println(m[i][j]); // 갱신되는 값 확인
+                }
+
+            }
+        }
+
+        System.out.println(m[1][n-1]); // answer
     }
 }
+
+
+
+
